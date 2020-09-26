8b50a4b44dc07b0d41b6c96f978e746393941784
Stephanie Chamblee <sc001t@att.com>
2020-09-26 09:15:26 -0400

    Add exit animation

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function initPage () {
-    console.log('init')
+    const icon = document.getElementsByTagName('img')[0];
     setTimeout(function() {
-        document.getElementsByTagName('img')[0].className = 'slide';
+        icon.classList.add('slide');
     }, 500)
+    document.getElementsByTagName('form')[0].addEventListener('submit', (e) => {
+        e.preventDefault();
+        icon.classList.add('exit');
+    })
 }
 
 initPage();
