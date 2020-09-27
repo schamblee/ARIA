f85c723debbbedf353c3eb7ba3e54d68e50f27cc
Stephanie Chamblee <sc001t@att.com>
2020-09-27 07:04:48 -0400

    Complete form example

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 function initPage () {
-    const icon = document.getElementsByTagName('img')[0];
+    const icon = document.getElementById('turtle');
+    const message = document.getElementById('message');
+    const form = document.getElementById('form');
+    const btn = document.getElementById('submitBtn');
+
+    form.addEventListener("change", () => {
+        btn.disabled = !form.checkValidity()
+    });
+
     setTimeout(function() {
         icon.classList.add('slide');
-    }, 500)
+    }, 500);
+
     document.getElementsByTagName('form')[0].addEventListener('submit', (e) => {
         e.preventDefault();
         icon.classList.add('exit');
-    })
+        setTimeout(function() {
+            message.classList.remove('hidden');
+            message.classList.add('slide');
+        }, 500);
+    });
 }
 
 initPage();
